17cb3e4db1a1eb793b0140034aadcbdccd067330
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-25 18:05:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ if mode_anak:
 # Gambar tiap bangun
 gambar_dict = {
     "Persegi": "images/persegi.png",
-    "Persegi Panjang": "images/persegi_panjang1.svg": "images/persegi_panjang2.png",
+    "Persegi Panjang": "images/persegi_panjang1.svg",
+    "Persegi Panjang": "images/persegi_panjang2.png",
     "Segitiga": "images/segitiga.png",
     "Lingkaran": "images/lingkaran.png",
     "Jajar Genjang": "images/jajar_genjang.png",

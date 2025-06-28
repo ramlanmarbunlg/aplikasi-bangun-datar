9edc178a12f7f1b73c2c2e82fc377c2919b39ce4
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-28 22:29:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -164,8 +164,7 @@ if st.session_state.mode_quiz:
             }
         ))
         fig.update_layout(height=250, width=400, margin=dict(t=30, b=10, l=10, r=10))
-        # Tampilkan countdown animasi lingkaran
-        st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig)
 
         jawaban_disabled = sisa_waktu == 0
         jawaban = st.radio("Pilih jawaban:", soal["opsi"], key=f"soal{indeks}", disabled=jawaban_disabled)

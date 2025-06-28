959cccb5654f66e0f0cd697085f3b8d40f5df5a8
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-29 02:14:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,9 +135,9 @@ if st.session_state.mode_quiz:
     indeks = st.session_state.quiz_index
 
     # Soal masih ada
-if indeks < total_soal:
-    soal = soal_data[indeks]
-    st.header(f"🎓 Quiz: {soal['kategori']} - Soal {indeks + 1} dari {total_soal}")
+    if indeks < total_soal:
+        soal = soal_data[indeks]
+        st.header(f"🎓 Quiz: {soal['kategori']} - Soal {indeks + 1} dari {total_soal}")
 
     # Progress bar visual soal
     progress = (indeks + 1) / total_soal

ffaf8572bc9840093082937abc17e74376d58b62
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-28 20:21:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -190,14 +190,14 @@ if st.session_state.mode_quiz:
         st.session_state.start_time = time.time()
         st.rerun()
 
-# ✅ Tombol untuk kembali ke mode kalkulasi
-if st.button("📐 Kembali ke Mode Kalkulasi"):
-    st.session_state.mode_quiz = False
-    st.session_state.quiz_index = 0
-    st.session_state.quiz_jawaban = {}
-    st.session_state.start_time = time.time()
-    st.rerun()
-st.stop()
+    # ✅ Tombol untuk kembali ke mode kalkulasi
+    if st.button("📐 Kembali ke Mode Kalkulasi"):
+        st.session_state.mode_quiz = False
+        st.session_state.quiz_index = 0
+        st.session_state.quiz_jawaban = {}
+        st.session_state.start_time = time.time()
+        st.rerun()
+    st.stop()
 
 # ============= MODE KALKULASI BANGUN DATAR=============
 # Gambar ilustrasi tiap bangun

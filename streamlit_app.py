15e5feb7405f0c8d977b019c04d5b62f9968ed64
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-28 20:20:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -190,6 +190,15 @@ if st.session_state.mode_quiz:
         st.session_state.start_time = time.time()
         st.rerun()
 
+# ✅ Tombol untuk kembali ke mode kalkulasi
+if st.button("📐 Kembali ke Mode Kalkulasi"):
+    st.session_state.mode_quiz = False
+    st.session_state.quiz_index = 0
+    st.session_state.quiz_jawaban = {}
+    st.session_state.start_time = time.time()
+    st.rerun()
+st.stop()
+
 # ============= MODE KALKULASI BANGUN DATAR=============
 # Gambar ilustrasi tiap bangun
 gambar_dict = {
@@ -314,15 +323,6 @@ link_dict = {
 }
 
 st.markdown(f"📚 [Baca materi lengkap tentang {bangun}]({link_dict[bangun]})")
-
-    # ✅ Tombol untuk kembali ke mode kalkulasi
-if st.button("📐 Kembali ke Mode Kalkulasi"):
-    st.session_state.mode_quiz = False
-    st.session_state.quiz_index = 0
-    st.session_state.quiz_jawaban = {}
-    st.session_state.start_time = time.time()
-    st.rerun()
-st.stop()
 
 # Footer
 st.markdown("---")

2bcc059fcb93aa5515a3a0cda18722d0d11fa136
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-28 22:15:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,8 +171,8 @@ if st.session_state.mode_quiz:
         ))
             # ✅ Atur ukuran grafik agar tidak terlalu besar
         fig.update_layout(
-            height=200,  # ganti sesuai keinginan (default biasanya 450–600)
-            width=100,
+            height=100,  # ganti sesuai keinginan (default biasanya 450–600)
+            width=500,
             margin=dict(t=30, b=10, l=10, r=10)
         )
     

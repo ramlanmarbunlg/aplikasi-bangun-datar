3c732c794abdbe6903d0b46f8b2683098d71303e
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-28 22:14:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,12 +171,10 @@ if st.session_state.mode_quiz:
         ))
             # ✅ Atur ukuran grafik agar tidak terlalu besar
         fig.update_layout(
-            height=300,  # ganti sesuai keinginan (default biasanya 450–600)
-            width=500,
+            height=100,  # ganti sesuai keinginan (default biasanya 450–600)
+            width=100,
             margin=dict(t=30, b=10, l=10, r=10)
         )
-    
-        st.plotly_chart(fig)
     
         # Tampilkan countdown animasi lingkaran
         st.plotly_chart(fig, use_container_width=True)
